9eaad9b92b6a4a2b031ef44b8628a72827781d82
Kristijan Horvatić <111249835+kristijaaaan-test@users.noreply.github.com>
2023-11-10 11:45:10 +0100

    Delete house-marketplace directory

--- house-marketplace/src/services/apiListings.js
+++
@@ -1,9 +0,0 @@
-import supabase from "./supabase";
-
-export async function getListings() {
-  const { data, error } = await supabase.from("listings").select("*");
-
-  if (error) throw new Error("Listings could not be loaded");
-
-  return data;
-}
